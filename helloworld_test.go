7c2a9ba7880aab632c841ca59c0627456a668e78
Divya Dadlani <ddadlani@pivotal.io>
2019-06-27 10:35:48 -0400

    Fix names
    Signed-off-by: Divya Dadlani <ddadlani@pivotal.io>

--- helloworld_test.go
+++ helloworld_test.go
@@ -3,13 +3,11 @@ package main_test
 import (
 	"os"
 	"testing"
-
-	sample "github.com/ddadlani/concourse-sample"
 )
 
 func TestHandler(t *testing.T) {
 	os.Setenv("TARGET", "blah")
-	sample.SetTarget("test")
+	SetTarget("test")
 	target := os.Getenv("TARGET")
 	if target != "test" {
 		t.Error("target was not set correctly")
